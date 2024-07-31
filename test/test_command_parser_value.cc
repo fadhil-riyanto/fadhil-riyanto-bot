4aa8add2d08d45567d5660f711101a9c94bb93bb
fadhil riyanto <me@fadev.org>
2024-07-31 10:16:46 +0700

    insert license header
    Signed-off-by: fadhil riyanto <me@fadev.org>

--- test/test_command_parser_value.cc
+++ test/test_command_parser_value.cc
@@ -1,3 +1,10 @@
+// SPDX-License-Identifier: GPL-3.0-only
+/*
+ *  Copyright (C) 2024  Fadhil Riyanto
+ * 
+ *  https://github.com/fadhil-riyanto/telegram-bot
+ */
+
 #include <cstdio>
 #include <gtest/gtest.h>
 
