9b2794a7806f69c344a2f4248fdab63eda48d661
fadhil riyanto <me@fadev.org>
2024-08-02 19:12:38 +0700

    add method, send_queue and its constructor
    Signed-off-by: fadhil riyanto <me@fadev.org>

--- src/utils/threading.cc
+++ src/utils/threading.cc
@@ -6,22 +6,52 @@
  */
 
 #include "../headers/threading.h"
+#include "../headers/int_helper.h"
 #include "../../submodule/log.c-patched/src/log.h"
 #include <cstdio>
 
 FadhilRiyanto::threading::thread_queue::thread_queue(int depth, struct queue_ring *ring)
 {
         ring->queue_list = new struct message_queue_data[depth];
+        ring->depth = depth;
         for(int i = 0; i < depth; i++) {
-                (ring->queue_list + i)->need_join = 0;
+                (ring->queue_list + i)->need_join = false;
+                (ring->queue_list + i)->state = false;
+                (ring->queue_list + i)->queue_num = -1;
         }
+
+        /* init counter */
+        this->counter = 0;
 };
+
+bool FadhilRiyanto::threading::thread_queue::send_queue(struct queue_ring *ring, TgBot::Message::Ptr message)
+{
+        for(int i = 0; i < ring->depth; i++) {
+                if ((ring->queue_list + i)->need_join == 0 && (ring->queue_list + i)->state == 0 &&
+                        (ring->queue_list + i)->queue_num == -1) {
+
+                        /* set current state to ring */
+                        this->counter = this->counter + 1;
+                        (ring->queue_list + i)->data = message;
+                        (ring->queue_list + i)->need_join = false;
+                        (ring->queue_list + i)->queue_num = this->counter;
+                        (ring->queue_list + i)->state = true;
+
+                        return true;
+                }
+        }
+
+        return false;
+}
 
 
 void FadhilRiyanto::threading::thread_helper::queue_debugger(int depth, struct queue_ring *ring)
 {
-        log_info("checking need_join_state");
+        log_info("checking all worker");
         for(int i = 0; i < depth; i++) {
-                log_debug("need_join %d: %s", i, (ring->queue_list + i)->need_join == 1 ? "true" : "false");
+                log_debug("qw %d | need_join %s, queue_num %d, state %s", i, 
+                        FadhilRiyanto::int_utils::int_helper::int2bool2str((ring->queue_list + i)->need_join).c_str(),
+                        (ring->queue_list + i)->need_join,
+                        FadhilRiyanto::int_utils::int_helper::int2bool2str((ring->queue_list + i)->state).c_str());
         }
 };
