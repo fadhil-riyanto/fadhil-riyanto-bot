46cb72fec96d974440648cf17cdc7a3b0dcf7f05
fadhil riyanto <me@fadev.org>
2024-08-05 10:50:41 +0700

    create handler class
    Signed-off-by: fadhil riyanto <me@fadev.org>

--- src/handler.cc
+++ src/handler.cc
@@ -4,3 +4,15 @@
  * 
  *  https://github.com/fadhil-riyanto/telegram-bot
  */
+
+#include "headers/handler.h"
+
+FadhilRiyanto::handler::handler(TgBot::Message::Ptr *message, TgBot::Bot *bot, struct ini_config *config,
+                        volatile std::sig_atomic_t *signal_status, struct ctx *ctx)
+{
+        this->message = message;
+        this->bot = bot;
+        this->config = config;
+        this->signal_status = signal_status;
+        this->ctx = ctx
+}
--- src/headers/handler.h
+++ src/headers/handler.h
@@ -4,3 +4,33 @@
  * 
  *  https://github.com/fadhil-riyanto/telegram-bot
  */
+
+#ifndef HANDLER_H
+#define HANDLER_H
+
+#include <tgbot/tgbot.h>
+#include "inih_parser.h"
+#include "ctx.h"
+#include <csignal>
+
+namespace FadhilRiyanto {
+
+class handler {
+private:
+        TgBot::Message::Ptr *message;
+        TgBot::Bot *bot;
+        struct ini_config *config;
+        volatile std::sig_atomic_t *signal_status;
+        struct ctx *ctx;
+
+public:
+        handler(TgBot::Message::Ptr *message, TgBot::Bot *bot, struct ini_config *config,
+                        volatile std::sig_atomic_t *signal_status, struct ctx *ctx);
+        void handle_command_input();
+        void handle_text();
+};
+
+
+} /* namespace */
+
+#endif
