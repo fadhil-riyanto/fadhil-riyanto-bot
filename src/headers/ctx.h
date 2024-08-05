249d86e3188c7ba883c6d31e19e30d233b152fb8
fadhil riyanto <me@fadev.org>
2024-08-05 10:51:31 +0700

    add ctx, reserved header
    Signed-off-by: fadhil riyanto <me@fadev.org>

---
+++ src/headers/ctx.h
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: GPL-3.0-only
+/*
+ *  Copyright (C) 2024  Fadhil Riyanto
+ * 
+ *  https://github.com/fadhil-riyanto/telegram-bot
+ */
+
+/* reserved */
+
+#ifndef CTX_H
+#define CTX_H
+
+struct ctx {
+        long    reserved;
+};
+
+#endif
