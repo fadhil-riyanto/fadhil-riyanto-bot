f6ced19a605aa6a459e4b1fde26d6e936f477f16
fadhil riyanto <me@fadev.org>
2024-08-08 22:41:09 +0700

    create definition class from _reg.cc to _reg.h
    Signed-off-by: fadhil riyanto <me@fadev.org>

---
+++ src/bot_module/main/headers/_reg.h
@@ -0,0 +1,24 @@
+#ifndef _MAIN_REG_H
+#define _MAIN_REG_H
+
+#include "../../../headers/command_parser.h"
+#include "../../../headers/base_module.h"
+#include "../../../headers/ctx.h"
+#include <tgbot/tgbot.h>
+#include <string>
+#include <fmt/core.h>
+
+class module_main : public FadhilRiyanto::mod::base_module {
+        TgBot::Bot *bot;
+        TgBot::Message::Ptr msg;
+        struct ini_config *config;
+        struct ctx *ctx;
+public:
+        int module_init(TgBot::Bot *bot, TgBot::Message::Ptr msg, 
+                                        struct ini_config *config, struct ctx *ctx,
+                                        struct FadhilRiyanto::string_utils::command_parser_result *res);
+
+        int module_next(std::string input);
+};
+
+#endif
