010e805393939a5b1c18461deb66b4d0823266d9
fadhil riyanto <me@fadev.org>
2024-08-15 10:04:34 +0700

    implement db.cc class
    Signed-off-by: fadhil riyanto <me@fadev.org>

--- src/db.cc
+++ src/db.cc
@@ -7,3 +7,26 @@
 
 #include <mongoc/mongoc.h>
 #include "headers/db.h"
+
+void FadhilRiyanto::db::db::setup_ptr(struct ctx *ctx)
+{
+        this->ctx = ctx;
+}
+
+void FadhilRiyanto::db::db::setup_conn(const char* uri)
+{
+        mongoc_init();
+        this->ctx->client = mongoc_client_new(uri);
+}
+
+void FadhilRiyanto::db::db::select(char* dbname, char* collname)
+{
+        this->ctx->coll = mongoc_client_get_collection(ctx->client, dbname, collname);
+}
+
+void FadhilRiyanto::db::db::clean()
+{
+        mongoc_collection_destroy(this->ctx->coll);
+        mongoc_client_destroy(this->ctx->client);
+        mongoc_cleanup();
+}
