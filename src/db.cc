7be35f8f55c9ebe1670207e40cf0b5da4d8588e5
fadhil riyanto <me@fadev.org>
2024-08-15 20:23:02 +0700

    test: add wrapper mongodb connection class
    Signed-off-by: fadhil riyanto <me@fadev.org>

--- src/db.cc
+++ src/db.cc
@@ -6,6 +6,7 @@
  */
 
 #include <mongoc/mongoc.h>
+#include <string>
 #include "headers/db.h"
 
 void FadhilRiyanto::db::db::setup_ptr(struct ctx *ctx)
@@ -19,9 +20,14 @@ void FadhilRiyanto::db::db::setup_conn(const char* uri)
         this->ctx->client = mongoc_client_new(uri);
 }
 
-void FadhilRiyanto::db::db::select(char* dbname, char* collname)
+mongoc_collection_t* FadhilRiyanto::db::db::select(std::string dbname, std::string collname)
 {
-        this->ctx->coll = mongoc_client_get_collection(ctx->client, dbname, collname);
+        return mongoc_client_get_collection(ctx->client, dbname.c_str(), collname.c_str());
+}
+
+mongoc_client_t* FadhilRiyanto::db::db::export_conn()
+{
+        return this->ctx->client;
 }
 
 void FadhilRiyanto::db::db::clean()
@@ -29,4 +35,26 @@ void FadhilRiyanto::db::db::clean()
         mongoc_collection_destroy(this->ctx->coll);
         mongoc_client_destroy(this->ctx->client);
         mongoc_cleanup();
+}
+
+void FadhilRiyanto::db::key_value::bind(FadhilRiyanto::db::db *db)
+{
+        this->dbptr = db;
+}
+
+void FadhilRiyanto::db::key_value::dump_all()
+{
+        const bson_t *doc;
+        mongoc_cursor_t *cursor;
+        char *str;
+
+        bson_t *query = bson_new();
+        cursor = mongoc_collection_find_with_opts(this->dbptr->select("test-db", "collection"), query, NULL, NULL);
+
+        while(mongoc_cursor_next(cursor, &doc)) {
+                str = bson_as_canonical_extended_json(doc, NULL);
+                printf ("%s\n", str);
+                bson_free(str);
+        }
+
 }
