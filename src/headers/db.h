0eb85ee5225976e787ba41b01ba4e34bf1d323be
fadhil riyanto <me@fadev.org>
2024-09-13 23:51:23 +0700

    commit: old struct data, which has been deactivated

--- src/headers/db.h
+++ src/headers/db.h
@@ -14,26 +14,7 @@
 
 namespace FadhilRiyanto::db {
 
-        struct ctx {
-                mongoc_client_t *client;
-                mongoc_collection_t *coll;
-                mongoc_cursor_t *cursor;
-                bson_error_t error;
-                bson_oid_t oid;
-                bson_t *doc;
-        };
-
-        class db {
-        private:
-                struct ctx *ctx;
-        public:
-                void setup_ptr(struct ctx *ctx);
-                /* https://github.com/fadhil-riyanto/mongodb-crud/blob/255fd5b8cbec55027a9c6d0e545fb0f13292e486/include/conn.h#L14 */
-                void setup_conn(const char* uri);
-                mongoc_client_t* export_conn();
-                mongoc_collection_t* select(std::string dbname, std::string collname); /* return ctx */
-                void clean();
-        };
+        
 
         class key_value {
         private:
